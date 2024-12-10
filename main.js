00495a5f27f219f9ef6ea4450ded3f3d8a1a722d
marslikbola <mengboyevfarrux5@gmail.com>
2024-12-11 00:24:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log(200);
+console.log( " Vasilliy");
+console.log(20);
+console.log("2010");
+console.log(" 101010");
+
+
+let box = 30
+console.log(box);
+
+let Box1 ="123456"
+console.log(Box1);
+
+let e
+let v = 10
+let t = -""
+e = v/t
+
+console.log(e);
+
+
+let y
+let s = 10
+let g = +"2,5"
+
+y = s/g
+console.log(y);
